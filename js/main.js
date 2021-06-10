2fd8b0b7c41aeb2f653e99a515b7e43e5bb7c069
AdaLollA <lorenz.graaf@gmail.com>
2021-06-10 17:00:14 +0200

    fixed navbar

--- js/main.js
+++ js/main.js
@@ -11,7 +11,21 @@ $(document).ready(function() {
         })
         return false
     })
-
     $('.project-area .button-group #btn1').trigger('click')
 
+    // sticky navigation menu
+    let nav_offset_top = $('.header-area').height()+50
+    function navbarFixed() {
+        if($('.header-area').length) {
+            $(window).scroll(function() {
+                let scroll = $(window).scrollTop()
+                if(scroll >= nav_offset_top) {
+                    $('.header-area .main-menu').addClass('navbar-fixed')
+                } else {
+                    $('.header-area .main-menu').removeClass('navbar-fixed')
+                }
+            })
+        }
+    }
+    navbarFixed()
 })
